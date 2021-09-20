9f62c979e0b121631e19c799df290e39fe7c0537
feiteng-gcp <li.feiteng1@gmail.com>
2021-09-20 09:40:23 -0400

    crawler fix

--- Crawlers.py
+++ Crawlers.py
@@ -103,7 +103,7 @@ def fetchContestRankingPage(contest):#, CNRegion = False, biweeklyContest = Fals
                 submissionResponse = submissionResponse.json()
                 break
             else:
-                logging.info(submissionResponse.text)
+                # logging.info(submissionResponse.text)
                 logger.info('Next wait time..' + str(sleep_time))
                 time.sleep(sleep_time)
                 sleep_time *= 2
@@ -183,8 +183,8 @@ def crawlSubmission_raw(contest, page_end):
                 if data_region == 'CN': submissionRequestURL = submissionURLCN
                 submissionRequestURL = submissionRequestURL % submission_id
 
-                logger.info("Crawling raw file [contest %s][page %s][counter %d][raw file %s][%.2f]" % (contest_str, page, submissionCounter, 
-                    submission_id, (submissionCounter / size) * 100))
+                logger.info("Crawling raw file [contest %s][page %s][counter %d][raw file %s][region %s][%.0f%%]" % (contest_str, page, submissionCounter, 
+                    submission_id, data_region, (submissionCounter / size) * 100))
 
                 # print(submission_id)
                 if submission_id in raw_files_crawled_JSON: 
@@ -199,15 +199,17 @@ def crawlSubmission_raw(contest, page_end):
                 
                 flag = True
                 while True:
-                    submissionResponse = requests.get(submissionRequestURL, headers = {'User-agent':'your bot 0.1'})
-                    if sleep_time > 5000: # waited over 32 seconds
+                    submissionResponse = requests.get(submissionRequestURL)
+                    if sleep_time > 50000: 
                         flag = False
                         break
                     elif submissionResponse.status_code == 200: 
                         submissionResponse = submissionResponse.json()
+                        break
                     else:
-                        logger.info(submissionResponse)
-                        logger.info(submissionResponse.text)
+                        logger.info('Request URL=' + str(submissionRequestURL))
+                        logger.info('Response Code=' + str(submissionResponse))
+                        logger.info('Response Text=' + str(submissionResponse.text))
                         logger.info('Next wait time..' + str(sleep_time))
                         time.sleep(sleep_time)
                         sleep_time *= 2
@@ -223,144 +225,5 @@ def crawlSubmission_raw(contest, page_end):
                 IO_Helper.writeJSON(submission_raw_filename, submissionResponse)
                 IO_Helper.writeJSON(file_name_crawled_raw, raw_files_crawled_JSON)  
     
-
-# def crawlSubmissions(contest, page_end, record_content):
-
-#     found_new_record = False
-#     record_content[contest] = collections.defaultdict(dict)
-#     # logging.info('parsing..')
-#     logging.basicConfig(
-#         level=logging.DEBUG,
-#         handlers=
-#         [
-#             logging.FileHandler('logging/Crawling.log'),
-#             logging.StreamHandler()
-#         ]
-#     )
-
-#     # record_content = {}# collections.defaultdict(dict)
-
-#     codingSuffix = {"cpp":"cpp",
-#                     "java":"java",
-#                     "python3":"py",
-#                     "python":"py",
-#                     "csharp":"cs",
-#                     "javascript":"js",
-#                     "golang":"go",
-#                     "rust":"rs",
-#                     "c":"c"
-#     }
-
-
-#     contestName = str(contest)
-#     cur_folder = str(pathlib.Path().resolve())
-#     submissionURL = "https://leetcode.com/api/submissions/%d"
-#     submissionURLCN = "https://leetcode-cn.com/api/submissions/%d"
-
-#     logging.info("Crawling submitted codes... %s " % contestName)
-
-#     # JSON_Location = 'C:/Users/lifeiteng/projects/visualizer/getRank/Contest JSON/' + contestName + '/'
-#     Ranking_Folder = cur_folder + '/Contest_Ranking/' + contestName + '/'
-#     outputLocation = cur_folder + '/Contest_Submission/' + contestName + '/'
-#     if not os.path.exists(outputLocation):
-#         os.makedirs(outputLocation)
-#     processedJSON = outputLocation + 'crawled.json'
-#     if not os.path.exists(processedJSON):
-#         f = open(processedJSON, 'w')
-#         f.write('{}')
-#         f.close()
-#     processedID = {}
-#     with open(processedJSON) as file:
-#         processedID = json.load(file)
-#     # assuming 500 pages, usually around 200
-#     for page in range(1, page_end):
-#         page_str = str(page)
-#         time.sleep(1)
-#         # try:
-#         Ranking_Page = Ranking_Folder + page_str + '.json'
-#         Ranking_Page_JSON = {}
-#         with open(Ranking_Page) as file:
-#             Ranking_Page_JSON = json.load(file)
-#         # except Exception as err:
-#         #     logging.info(err)
-#         #     break
-
-#         submissions = Ranking_Page_JSON['submissions']
-#         total_rank = Ranking_Page_JSON['total_rank']
-
-#         for user in range(len(total_rank)):
-
-#             line = total_rank[user]
-
-#             submission = submissions[user]
-#             data_region = line['data_region']
-#             username = line['username']
-#             userrank = line['rank']
-#             for question_num in submission:
-#                 # try:
-#                 uniqueID = str(userrank) +  '_' + username + '_' + str(question_num)
-#                 cur_time = datetime.now(pytz.timezone('America/New_York'))
-#                 if question_num not in record_content[contest]:
-#                     record_content[contest][question_num] = 0
-#                 record_content[contest][question_num] += 1
-#                 if uniqueID in processedID: 
-#                     logging.info("[%s][Contest=%s][page=%d][question=%s] submission exists" % (cur_time, contestName, page, question_num))
-                    
-#                     continue
-#                 # if i % 20 == 0: 
-#                 logging.info("[%s][Contest=%s][Crawling page=%d][user=%d] question num.. %s"  % (cur_time, contestName, page, user, question_num))
-#                 found_new_record = True
-#                 processedID[uniqueID] = 1
-#                 kth_submission = submission[question_num]
-#                 submission_id = kth_submission['submission_id']
-                
-#                 submissionRequestURL = submissionURL
-
-#                 if data_region == 'CN': submissionRequestURL = submissionURLCN
-
-#                 submissionRequestURL = submissionRequestURL % submission_id
-
-#                 sleep_time = 1
-#                 while True:
-#                     submissionResponse = requests.get(submissionRequestURL)
-#                     # logging.info(submissionResponse.status_code)
-#                     if sleep_time > 100:
-#                         break
-#                     elif submissionResponse.status_code == 200: 
-#                         submissionResponse = submissionResponse.json()
-#                         break
-#                     else:
-#                         logging.info(submissionResponse.text)
-#                         print(submissionResponse)
-#                         print('next wait time..' + str(sleep_time))
-#                         time.sleep(sleep_time)
-#                         sleep_time *= 2
-#                 try:
-#                     coding_content =  submissionResponse['code']
-#                     coding_language = submissionResponse['lang']
-                    
-
-#                     if coding_language not in codingSuffix: codingSuffix[coding_language] = coding_language
-#                     coding_language = codingSuffix[coding_language]
-                    
-#                     # print(record_content)
-#                     fileLocation = outputLocation + coding_language + '/' + str(question_num)
-#                     # logging.info(fileLocation)
-#                     if not os.path.exists(fileLocation):
-#                         os.makedirs(fileLocation)
-
-#                     # save as contest - code language - [username][code content]
-#                     filename = fileLocation + '/' + str(userrank) + '_' + username + '.' + coding_language
-#                     if os.path.exists(filename): continue
-#                     file = open(filename, 'w', encoding='utf-8')
-#                     file.write(coding_content)
-#                     file.close()
-#                 except Exception as err:
-#                     print(err)
-#                     pass
-#             with open(processedJSON, 'w') as outputFile:
-#                 json.dump(processedID, outputFile)        
-            
-#     # logging.shutdown()
-#     print(record_content)
-#     return found_new_record
+if __name__ == '__main__':
+    crawlSubmission_raw(89, 21)
